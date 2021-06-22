288727eee3cd7393ebedd890dff90e1faa6e775f
RamiZregat <rami.zregat97@gmail.com>
2021-06-22 11:35:34 +0300

    fixing lower case

--- JS/app.js
+++ JS/app.js
@@ -5,42 +5,42 @@ let UserName = prompt('Please write your name here so we can know each other bet
 alert('Hello ' + UserName + ' It\'s an honor to meet you')
 alert('I want to play with you a guessing game about my personalization hope you enjoy it')
 
-let MyAge = prompt('Am I older than 25 \(y/n\)')
+let MyAge = prompt('Am I older than 25 \(y/n\)').toLowerCase()
 switch (MyAge) {
-    case MyAge = 'y'.toLowerCase():
+    case MyAge='y':
         alert('You are wrong I\'m actaully 24 years old it\'s okay lets move on to another question')
         break;
-    case MyAge = 'n'.toLowerCase():
+    case MyAge = 'n':
         alert('Wow you guessed it right I\'m actually 24 years old')
         break;
     default:
         alert('Really ' + UserName + '... it was a yes or no question u suppose to answer it with y for yes or n for no okay whatever lets move on to next question')
 
 }
-let MyHobby1 = prompt('Am I a gamer? \(y/n\)')
-if (MyHobby1 == 'y'.toLowerCase()) {
+let MyHobby1 = prompt('Am I a gamer? \(y/n\)').toLowerCase()
+if (MyHobby1 == 'y') {
     alert('Wow you guessed it right I am a gamer')
-} else if (MyHobby1 == 'n'.toLowerCase()) {
+} else if (MyHobby1 == 'n') {
     alert('You are wrong I\'m actually a gamer it\'s okay lets move on to another question')
 } else {
     alert('Really ' + UserName + '... it was a yes or no question u suppose to answer it with y for yes or n for no okay whatever lets move on to next question')
 }
 
-let MyHobby2 = prompt('Am I into programming? \(y/n\)')
+let MyHobby2 = prompt('Am I into programming? \(y/n\)').toLowerCase()
 switch (MyHobby2) {
-    case MyHobby2 = 'y'.toLowerCase():
+    case MyHobby2 = 'y':
         alert('Wow you guessed it right I am into porgramming.')
         break;
-    case MyHobby2 = 'n'.toLowerCase():
+    case MyHobby2 = 'n':
         alert('You are wrong I\'m actaully into programming it\'s okay lets move on to another question.')
         break;
     default:
         alert('Really ' + UserName + '... it was a yes or no question u suppose to answer it with y for yes or n for no okay whatever lets move on to next question.')
 }
-let MyHobby3 = prompt('Am I into Sport exercises? \(y/n\)')
-if (MyHobby3 == 'y'.toLowerCase()) {
+let MyHobby3 = prompt('Am I into Sport exercises? \(y/n\)').toLowerCase()
+if (MyHobby3 == 'y') {
     alert('Wow you guessed it right I am into Sport exercises.')
-} else if (MyHobby3 == 'n'.toLowerCase()) {
+} else if (MyHobby3 == 'n') {
     alert('You are wrong I\'m actually into Sport exercises it\'s okay.')
 } else {
     alert('Really ' + UserName + '... it was a yes or no question u suppose to answer it with y for yes or n for no okay whatever.')
